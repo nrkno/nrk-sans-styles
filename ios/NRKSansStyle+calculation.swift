2df0907f2bcc846582b8bf7446027c6094ecc9f5
Joakim Stien <joakim.stien@icloud.com>
2022-04-26 14:44:11 +0200

    Added docs

--- ios/NRKSansStyle+calculation.swift
+++ ios/NRKSansStyle+calculation.swift
@@ -3,6 +3,28 @@ import UIKit
 
 public extension FontStyle {
     public var uiFontWeight: UIFont.Weight {
+        /// The relationship between the "standard" font weights (the ones that are
+        /// used by Figma, web, and Android) and the font weights used by iOS are
+        /// not linear. Because the relationship is unknown, we approximate the
+        /// value by interpolating between the closest known anchors.
+        ///
+        /// Note that even the anchors themselves are not necessarily correct. They
+        /// SEEM to be correct, based on the following correlation between `UIFont.Weight`,
+        /// `UIFont.Weight.rawValue`, figma weights, and `UIImage.SymbolWeight` - in
+        /// addition to manual inspection of course.
+        ///
+        ///  UIFont.Weight   rawValue    Figma    UIImage.SymbolWeight
+        ///  ----------------------------------------------------------
+        ///  ultraLight      -0.8        100        1
+        ///  thin            -0.6        150        2
+        ///  light           -0.4        300        3
+        ///  regular          0          400        4
+        ///  semibold         0.3        600        5
+        ///  bold             0.4        700        6
+        ///  heavy            0.56       800        7
+        ///  black            0.62       900        8
+        ///
+        /// The anchor-array maps from standard font weights to 'UIFont.Weight'.
         let anchors: [(Int, Float)] = [
             (100, -0.8),
             (150, -0.6),
@@ -30,11 +52,20 @@ public extension FontStyle {
     }
 
     public var lineHeightMultiple: CGFloat? {
+        /// The NSAttributedString property 'lineHeightMultiple' is a multiple
+        /// of the **DEFAULT** line height, embedded in the font. For NRK Sans, the
+        /// default line height is 120% of the point size. So the lineHeightMultiple
+        /// property is a factor on 120%. If *NO* space between the lines is desired,
+        /// lineHeightMultiple must be 0.833, because 0.833 * 120 = 100.
         guard let lineHeightEm = lineHeightEm else { return nil }
         return lineHeightEm / 1.2
     }
 
     public var kerning: CGFloat? {
+        /// Kerning/tracking/letter spacing is also using a custom value on iOS (I hope
+        /// you're not surprised at this point). Where other platforms use an 'em' property,
+        /// iOS uses a value that's fixed to the point size. Multiply the point size with the
+        /// 'letter spacing em', and you get the correct kerning value.
         guard let letterSpacingEm = letterSpacingEm else { return nil }
         return pointSize * letterSpacingEm
     }
